b8b45930ed3e7234ece993b4ac1a10aedd86b642
me-cafebabe <me_cafebabe@hotmail.com>
2022-09-01 14:32:55 +0800

    MiThoriumSSI: Add PHONY target to build DSU ZIP archive

--- Android.mk
+++ Android.mk
@@ -9,4 +9,13 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),MiThoriumSSI)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
+INSTALLED_MITHORIUMSSI_DSU_TARGET := $(PRODUCT_OUT)/mithoriumssi_dsu.zip
+
+$(INSTALLED_MITHORIUMSSI_DSU_TARGET): $(PRODUCT_OUT)/system.img $(PRODUCT_OUT)/vbmeta.img
+	$(call pretty,"Target Mi-Thorium DSU ZIP archive: $@")
+	$(hide) /usr/bin/zip -qjX $@ $(PRODUCT_OUT)/system.img $(PRODUCT_OUT)/vbmeta.img
+
+.PHONY: mithoriumssi_dsu
+mithoriumssi_dsu: $(INSTALLED_MITHORIUMSSI_DSU_TARGET)
+
 endif
