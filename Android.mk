a23b78ef7299aef932be9a4fb1e752b85a7999e4
me-cafebabe <me_cafebabe@hotmail.com>
2022-09-01 21:25:25 +0800

    MiThoriumSSI: Add BUILD_ID to artifact name

--- Android.mk
+++ Android.mk
@@ -9,7 +9,7 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),MiThoriumSSI)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
-INSTALLED_MITHORIUMSSI_DSU_TARGET := $(PRODUCT_OUT)/mithoriumssi_dsu.zip
+INSTALLED_MITHORIUMSSI_DSU_TARGET := $(PRODUCT_OUT)/mithoriumssi_dsu-$(BUILD_ID).zip
 
 $(INSTALLED_MITHORIUMSSI_DSU_TARGET): $(PRODUCT_OUT)/system.img $(PRODUCT_OUT)/vbmeta.img
 	$(call pretty,"Target Mi-Thorium DSU ZIP archive: $@")
