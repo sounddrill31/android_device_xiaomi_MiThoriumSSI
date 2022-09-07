46c0b5aeae8ca87620b0e04c362a816b9387664c
me-cafebabe <me_cafebabe@hotmail.com>
2022-09-08 02:35:32 +0800

    Fluid: MiThoriumSSI: Project Fluid Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -11,3 +11,11 @@ COMMON_LUNCH_CHOICES := \
     lineage_MiThoriumSSI-user \
     lineage_MiThoriumSSI-userdebug \
     lineage_MiThoriumSSI-eng
+
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/fluid_MiThoriumSSI.mk
+
+COMMON_LUNCH_CHOICES += \
+    fluid_MiThoriumSSI-user \
+    fluid_MiThoriumSSI-userdebug \
+    fluid_MiThoriumSSI-eng
---
+++ fluid_MiThoriumSSI.mk
@@ -0,0 +1,37 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit some common Fluid stuff.
+$(call inherit-product, vendor/fluid/config/common_full_phone.mk)
+
+# Inherit from MiThoriumSSI device
+$(call inherit-product, device/xiaomi/MiThoriumSSI/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := MiThoriumSSI
+PRODUCT_NAME := fluid_MiThoriumSSI
+BOARD_VENDOR := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi-Thorium Shared System Image
+PRODUCT_MANUFACTURER := Xiaomi
+TARGET_VENDOR := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="land-user 6.0.1 MMB29M V10.2.2.0.MALMIXM release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "Xiaomi/land/land:6.0.1/MMB29M/V10.2.2.0.MALMIXM:user/release-keys"
+
+# MiThoriumSSI
+MITHORIUMSSI_ROM_NAME := ProjectFluid
+MITHORIUMSSI_ROM_VERSION := 2.7
