ca25b179acfd96f3c4657b06b959b8bfa9e1a14d
me-cafebabe <me_cafebabe@hotmail.com>
2022-10-06 18:02:33 +0800

    Miku: MiThoriumSSI: Don't call me BuildBot please :)

--- miku_MiThoriumSSI.mk
+++ miku_MiThoriumSSI.mk
@@ -32,6 +32,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "Xiaomi/land/land:6.0.1/MMB29M/V10.2.2.0.MALMIXM:user/release-keys"
 
+# Miku
+MIKU_MASTER := 0xCAFEBABE
+
 # MiThoriumSSI
 MITHORIUMSSI_ROM_NAME := MikuUI
 MITHORIUMSSI_ROM_VERSION := TDA
